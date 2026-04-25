18a31214472cb59ddc38de47b00d856ec961665d
lordydill <ahamdfadil846@gmail.com>
2026-04-25 12:00:53 +0700

    update auth dan scm fitur

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,20 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('barang')
+  getBarang(): string {
+    return 'bensing pertamina bulukumba';
+  }
+
+  @Post('bensing/masuk')
+  barangMasuk(): string {
+    return 'bensin masuk';
+  }
+
+  @Post('bensing/keluar')
+  barangKeluar(): string {
+    return 'bensing keluar';
   }
 }
--- src/auth/service/auth.service.ts
+++ src/auth/service/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login(data: any) {
+    const { username, password } = data;
+
+    if (!username || !password) {
+      return { status: false, message: 'Invalid input' };
+    }
+
+    if (username === 'admin' && password === '123') {
+      return { status: true, message: 'Success' };
+    }
+
+    return { status: false, message: 'Failed' };
+  }
+}
